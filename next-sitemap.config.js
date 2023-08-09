7e78bd44bb23445ee3dc08df9b4638365c236a2f
Teo Stocco <teo@zifeo.com>
2023-08-09 10:50:41 +0200

    feat: disallow gpt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,5 +5,8 @@ const siteUrl = process.env.SITE_URL || "http://localhost:3000";
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "GPTBot", disallow: "/" }],
+  },
   outDir: "out",
 };
